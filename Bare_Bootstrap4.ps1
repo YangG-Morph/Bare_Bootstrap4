fc35d4d3a91fce324ef481e554d87ac3cf478abb
YangG-Morph <38061493+YangG-Morph@users.noreply.github.com>
2020-05-30 16:25:50 -0500

    Update Bare_Bootstrap4.ps1

--- Bare_Bootstrap4.ps1
+++ Bare_Bootstrap4.ps1
@@ -2,9 +2,13 @@
 #Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/YangG-Morph/Bare_Bootstrap4/master/Bare_Bootstrap4.ps1)
 #TODO: Add variable for bootstrap_bare
 
-npm install startbootstrap-resume jquery browser-sync gulp gulp-sass merge-stream del popper.js del
-Get-ChildItem node_modules\startbootstrap-resume | Move-Item -Destination .\ -ErrorAction SilentlyContinue
+param(
+	[string]$bootstrap
+)
+
+npm install $bootstrap jquery browser-sync gulp gulp-sass merge-stream del popper.js del
+Get-ChildItem node_modules\$bootstrap | Move-Item -Destination .\ -ErrorAction SilentlyContinue
 npm install
 npm audit fix
 
-Start-BitsTransfer -Source "https://raw.githubusercontent.com/YangG-Morph/Bare_Bootstrap4/master/gulpfile.js" -Destination gulpfile.js -Asynchronous
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/YangG-Morph/Bare_Bootstrap4/master/gulpfile.js" -Destination ".\"
