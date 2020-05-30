af0fd205c20653bb46ab3c70610e62b200843d4b
YangG-Morph <38061493+YangG-Morph@users.noreply.github.com>
2020-05-29 19:25:57 -0500

    Update Bare_Bootstrap4.ps1

--- Bare_Bootstrap4.ps1
+++ Bare_Bootstrap4.ps1
@@ -1,4 +1,4 @@
 #Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/YangG-Morph/Bare_Bootstrap4/master/Bare_Bootstrap4.ps1)
 
-npm install startbootstrap-bare jquery browser-sync gulp sass merge-stream del
+npm install startbootstrap-bare jquery browser-sync gulp sass merge-stream del popper.js
 Get-ChildItem node_modules\startbootstrap-bare | Move-Item -Destination .\ -ErrorAction SilentlyContinue
