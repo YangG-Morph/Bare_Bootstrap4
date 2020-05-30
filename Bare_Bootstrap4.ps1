bea1f214032cfbd93c949598d2699ec2ab6abd5a
YangG-Morph <38061493+YangG-Morph@users.noreply.github.com>
2020-05-30 16:01:58 -0500

    Update Bare_Bootstrap4.ps1

--- Bare_Bootstrap4.ps1
+++ Bare_Bootstrap4.ps1
@@ -2,7 +2,7 @@
 #Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/YangG-Morph/Bare_Bootstrap4/master/Bare_Bootstrap4.ps1)
 #TODO: Add variable for bootstrap_bare
 
-npm install jquery startbootstrap-resume browser-sync gulp gulp-sass merge-stream del popper.js del
+npm install startbootstrap-resume jquery browser-sync gulp gulp-sass merge-stream del popper.js del
 Get-ChildItem node_modules\startbootstrap | Move-Item -Destination .\ -ErrorAction SilentlyContinue
 npm install
 npm audit fix
