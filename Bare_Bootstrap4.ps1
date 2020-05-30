cd39a821dd1059704f9f4c96125c8b9b91a4511f
YangG-Morph <38061493+YangG-Morph@users.noreply.github.com>
2020-05-29 20:01:03 -0500

    Update Bare_Bootstrap4.ps1

--- Bare_Bootstrap4.ps1
+++ Bare_Bootstrap4.ps1
@@ -1,6 +1,10 @@
+#Copy Invoke into Powershell
 #Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/YangG-Morph/Bare_Bootstrap4/master/Bare_Bootstrap4.ps1)
 
 npm install startbootstrap-bare jquery browser-sync gulp sass merge-stream del popper.js
 Get-ChildItem node_modules\startbootstrap-bare | Move-Item -Destination .\ -ErrorAction SilentlyContinue
 npm install
 npm audit fix
+
+#Set project to open up with Chrome
+((Get-Content -path gulpfile.js) -replace 'port: 3000','port: 3000,browser: ["Chrome"]') | Set-Content -Path gulpfile.js
