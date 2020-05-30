83096738338390d5f40bdfa4b8d0fc15245c72f2
YangG-Morph <38061493+YangG-Morph@users.noreply.github.com>
2020-05-30 15:40:29 -0500

    Update Bare_Bootstrap4.ps1

--- Bare_Bootstrap4.ps1
+++ Bare_Bootstrap4.ps1
@@ -1,7 +1,7 @@
 #Copy Invoke into Powershell
 #Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/YangG-Morph/Bare_Bootstrap4/master/Bare_Bootstrap4.ps1)
 
-npm install startbootstrap-bare jquery browser-sync gulp sass merge-stream del popper.js
+npm install jquery browser-sync gulp sass merge-stream del popper.js
 Get-ChildItem node_modules\startbootstrap-bare | Move-Item -Destination .\ -ErrorAction SilentlyContinue
 npm install
 npm audit fix
